44eff1112e4447c01ac9558d9c5a25c8e6a02755
wpgreene <wpgreene@ncsu.edu>
2022-01-08 00:08:25 -0500

    Appending start of template files

--- src/App.vue
+++ src/App.vue
@@ -20,7 +20,25 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;700&display=swap');
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: "Inter", sans-serif;
+  font-size: 16px;
+  width: 100vw;
+  min-width: 100vw;
+  height: 100vh;
+  min-height: 100vh;
+}
+
 #app {
-  
+  width: 100%;
+  height: 100%;
 }
 </style>
